dedad7ec98d58b18667121e7e28a1f8d95e9082e
ZachSullivan <zach.r.sullivan@gmail.com>
2015-10-23 16:15:02 -0400

    Agent will now wander around when working

--- Assets/Prefabs/06_Prefabs/WorkLogic.cs
+++ Assets/Prefabs/06_Prefabs/WorkLogic.cs
@@ -9,16 +9,80 @@ public class WorkLogic : MonoBehaviour {
 
 	AIFollow aiFollow;
 
+	public enum agentState
+	{
+		Wandering,
+		Hungry,
+		Working,
+		Default
+	}
+
+	public agentState aState;
 
 	void Start(){
 		target = GameObject.FindGameObjectWithTag ("WorkWaypoint").transform;
 
 		aiFollow = GetComponent<AIFollow>();
 
-		aiFollow.target = target;
+		//When an agent spawns, he should start by wandering
+		aState = agentState.Working;
+
+	}
+
+	void Update() {
+		switch (aState) {
+		case agentState.Wandering:
+
+			break;
+		case agentState.Hungry:
+						
+			break;
+			
+		case agentState.Working:
+			
+			aiFollow.target = target;
+			//aState = agentState.Working;
+			break;
+		case agentState.Default:
+			print("Default reached in AgentFSM Update");
+			break;
+		}
+	}
+
+	bool PointInsideSphere(Vector3 point, Vector3 center, float radius) {
+		print ("Dis" + Vector3.Distance(point, center));
+		print ("inside: " + (Vector3.Distance(point, center) < radius));
+		return Vector3.Distance(point, center) < radius;
 	}
 
 	public void TargetReached(){
-		print ("Here");
+
+		if(aState == agentState.Working){
+
+
+			if(PointInsideSphere(transform.position,GameObject.FindGameObjectWithTag ("WorkWaypoint").transform.position, 1.0f) != true){
+				target = GameObject.FindGameObjectWithTag ("WorkWaypoint").transform;
+				aiFollow.target = target;
+			}else{
+				//target.position = new Vector3 (Random.Range (-2, 2), 0, Random.Range (-2, 2));
+				int tempPosx = Random.Range(-2, 2);
+				int tempPosz = Random.Range(-2, 2);
+				target.position = new Vector3(transform.position.x + tempPosx,transform.position.y,transform.position.z + tempPosz);
+			}
+
+			aState = agentState.Working;
+		}
+
+		/*
+		 * if State1 
+		 * 	do this
+		 * if State2
+		 * 	do this
+		 * if State3
+		 * 	do this
+		 * if State4
+		 * 	do this
+		 * 
+		 */ 
 	}
 }
