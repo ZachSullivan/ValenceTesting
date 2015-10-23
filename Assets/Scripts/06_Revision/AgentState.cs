8c78013f45fbfafe19e706d981df0faab207e9e9
ZachSullivan <zach.r.sullivan@gmail.com>
2015-10-23 12:26:38 -0400

    Updated Updated agent state to use a statemachine

--- Assets/Scripts/06_Revision/AgentState.cs
+++ Assets/Scripts/06_Revision/AgentState.cs
@@ -11,25 +11,66 @@ public class AgentState : MonoBehaviour {
 	public bool feeding = false;
 	
 	public int hungerSearch = 90;
-	
-	void Awake () {
+
+	bool hasFinished = true;
+
+	/*void Awake () {
 		feeding = false;
 		foodSource = GameObject.FindWithTag("Foodsource");
 		_agentController = gameObject.GetComponent<AgentController>();
 		//Increase the agent's hunger level every 5 seconds
 		
+	}*/
+
+	//Used to control the movement of the agent
+	AgentPathfinder _agentPathfinder;
+	
+	//Create a series of possible states for the agent to be in
+	enum agentState
+	{
+		Wandering,
+		Hungry
 	}
 	
+	agentState aState;
+	
+	void Awake() {
+		
+		//Find the agent movement controller attached
+		_agentPathfinder = GetComponent<AgentPathfinder>();
+		
+		//When an agent spawns, he should start by wandering
+		aState = agentState.Wandering;
+		
+		//When an agent spawns, start updating his hunger level
+		updateHunger();
+	}
+
 	void Start(){
 		//StartCoroutine(AgentHunger());
 		InvokeRepeating("AgentHunger", 1, 1);
 	}
+
 	
-	void Update(){
-		
+	void Update() {
+		switch (aState) {
+		case agentState.Wandering:
+			Wander();
+			aState = agentState.Wandering;
+			break;
+		case agentState.Hungry:
+
+			if(!feeding){
+				_agentController.hungry = true;
+			}
+
+			break;
+		default:
+			print("Default reached in AgentFSM Update");
+			break;
+		}
 	}
-	
-	
+
 	//IEnumerator AgentHunger() {
 	void AgentHunger(){
 		if(!feeding){
@@ -45,8 +86,8 @@ public class AgentState : MonoBehaviour {
 			
 			if (hungerValue <= hungerSearch)
 			{
-				
-				_agentController.hungry = true;
+				aState = agentState.Hungry;
+				//_agentController.hungry = true;
 				//gameObject.GetComponent<TestMovement>().hungry = true;
 			}
 		}
@@ -76,13 +117,34 @@ public class AgentState : MonoBehaviour {
 	
 	public void Feed(){
 		feeding = true;
-
+		
 		//if(hungerValue < 100){
-			_agentController.hungry = false;
-			//hungerValue ++;
-			
+		_agentController.hungry = false;
+		//hungerValue ++;
+		
 		//} 	
 		
+
+	}
+	
+
+
+	
+	void Wander() {
+		print("Currently Wandering");
+		if (hasFinished == true) {
+			_agentController.target = new Vector3 (Random.Range (-50, 50), 0, Random.Range (-50, 50));
+			hasFinished = false;
+		}
+	}
+	
+	void SearchForFood() {
+		print("Currently Hungry");
+		_agentPathfinder.aState = AgentPathfinder.actionState.Hungry;
+	}
+	
+	void updateHunger() {
 		
 	}
+	
 }
